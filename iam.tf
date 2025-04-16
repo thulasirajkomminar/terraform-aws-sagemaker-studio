fa5745b18c996d1e177a023b130aad00187c0718
Francisco Bobadilla <francisco.bobadilla@southernlights.tech>
2025-04-16 15:33:48 -0300

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,6 +1,9 @@
 data "aws_iam_policy_document" "assume_policy" {
   statement {
-    actions = ["sts:AssumeRole"]
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
     principals {
       type = "Service"
       identifiers = [
